b6d303ef0beefd02e810a9b418c2e393544fcad9
joaoscioli <joaoscioli@outlook.com>
2024-11-11 08:10:55 -0300

    docs(selectionSort) add documentation project SelectionSort

--- java_50_exercises/src/sorting_by_selection_in_java/SelectionSort.java
+++ java_50_exercises/src/sorting_by_selection_in_java/SelectionSort.java
@@ -1,8 +1,30 @@
 package sorting_by_selection_in_java;
 
 import java.util.Scanner;
-
+/// SelectionSort
+///
+/// This class provides an implementation of the selection sort algorithm for sorting an array of integers.
+/// It includes a `main` method to demonstrate sorting a user-provided array.
+///
+/// Example:
+/// ```java
+/// Enter the size of the table: 5
+/// Enter the elements of the table:
+/// 34 12 24 9 5
+/// Table sorted:
+/// 5 9 12 24 34
+/// ```
+///
+/// The code reads the size and elements of an array from user input, sorts it using the `selectionSort` method, and
+/// the sorted array.
 public class SelectionSort {
+    /// Main method to execute the selection sort demonstration.
+    ///
+    /// - Reads an integer array from user input.
+    /// - Sorts the array using selection sort.
+    /// - Prints the sorted array.
+    ///
+    /// @param args Command-line arguments (not used).
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -23,6 +45,16 @@ public class SelectionSort {
         }
     }
 
+    /// Sorts an array in ascending order using the selection sort algorithm.
+    ///
+    /// The selection sort algorithm duvides the array into a sorted and an unsorted section.
+    /// It repeatedly selects the minimum element from the  unsorted section and moves it to the end of the sorted
+    /// section.
+    ///
+    /// - **Time Complexity**: `O(n^2)`, where `n`is the number of elements in the array.
+    /// - **Space Complexity**: `O(1)`, as it is an in-place sorting algorithm.
+    ///
+    /// @param arr The array of integers to be sorted.
     public static void selectionSort(int[] arr) {
         int n = arr.length;
         for (int i = 0; i < n-1; i++) {
