f6215d3b1def28bdb8795ae6e6c2d96d1f740f91
joaoscioli <joaoscioli@outlook.com>
2024-12-21 20:19:21 -0300

    feat(modify project) modify project PrimMinimumSpanningTree

--- java_50_exercises/src/primsAlgorithmMinimalSpanningTree_in_java/PrimMinimumSpanningTree.java
+++ java_50_exercises/src/primsAlgorithmMinimalSpanningTree_in_java/PrimMinimumSpanningTree.java
@@ -2,14 +2,16 @@ package primsAlgorithmMinimalSpanningTree_in_java;
 
 import java.util.ArrayList;
 import java.util.PriorityQueue;
+
 public class PrimMinimumSpanningTree {
     public static void main(String[] args) {
         // Representation of the undirected weighted graph as an adjacency list
         int numNodes = 5;
         ArrayList<ArrayList<Node>> graph = new ArrayList<>(numNodes);
         for (int i = 0; i < numNodes; i++) {
-            graph.add(new ArrayList<Node>());
+            graph.add(new ArrayList<>());
         }
+
         // Add graph edges with weights
         addWeightedEdge(graph, 0, 1, 2);
         addWeightedEdge(graph, 0, 2, 3);
@@ -18,8 +20,10 @@ public class PrimMinimumSpanningTree {
         addWeightedEdge(graph, 2, 3, 5);
         addWeightedEdge(graph, 2, 4, 2);
         addWeightedEdge(graph, 3, 4, 7);
+
         // Perform Prim's algorithm to find the minimal spanning tree
         ArrayList<Edge> minimumSpanningTree = prim(graph);
+
         // Display the edges of the minimal spanning tree and their total weight
         int totalWeight = 0;
         System.out.println("Edges of the minimal spanning tree:");
@@ -29,74 +33,75 @@ public class PrimMinimumSpanningTree {
         }
         System.out.println("Total weight of the minimal spanning tree: " + totalWeight);
     }
+
     public static void addWeightedEdge(ArrayList<ArrayList<Node>> graph, int node1, int node2, int weight) {
         graph.get(node1).add(new Node(node2, weight));
         graph.get(node2).add(new Node(node1, weight));
     }
+
     public static ArrayList<Edge> prim(ArrayList<ArrayList<Node>> graph) {
         int numNodes = graph.size();
         ArrayList<Edge> minimumSpanningTree = new ArrayList<>();
         PriorityQueue<Edge> priorityQueue = new PriorityQueue<>();
         boolean[] visited = new boolean[numNodes];
+
         // Start with any node
         int startNode = 0;
         visited[startNode] = true;
         for (Node neighbor : graph.get(startNode)) {
             priorityQueue.add(new Edge(startNode, neighbor.node, neighbor.weight));
         }
+
         while (minimumSpanningTree.size() < numNodes - 1) {
             Edge edge = priorityQueue.poll();
+            if (edge == null) break;
+
             int node1 = edge.node1;
             int node2 = edge.node2;
+
             if (visited[node1] && visited[node2]) {
-                continues;
+                continue;
             }
-            visited[node1] = true;
-            visited[node2] = true;
+
+            // Add the edge to the minimum spanning tree
             minimumSpanningTree.add(edge);
-            for (Node neighbor : graph.get(node2)) {
+
+            // Mark the new node as visited
+            int newNode = visited[node1] ? node2 : node1;
+            visited[newNode] = true;
+
+            // Add edges connected to the new node
+            for (Node neighbor : graph.get(newNode)) {
                 if (!visited[neighbor.node]) {
-                    priorityQueue.add(new Edge(node2, neighbor.node, neighbor.weight));
+                    priorityQueue.add(new Edge(newNode, neighbor.node, neighbor.weight));
                 }
             }
         }
+
         return minimumSpanningTree;
     }
+
     static class Node {
         int node;
         int weight;
+
         Node(int node, int weight) {
-            int node2 = edge.node2;
-            if (visited[node1] && visited[node2]) {
-                continues;
-            }
-            visited[node1] = true;
-            visited[node2] = true;
-            minimumSpanningTree.add(edge);
-            for (Node neighbor : graph.get(node2)) {
-                if (!visited[neighbor.node]) {
-                    priorityQueue.add(new Edge(node2, neighbor.node, neighbor.weight));
-                }
-            }
-        }
-        return minimumSpanningTree;
-    }
-    static class Node {
-        int node;
-        int weight;
-        Node(int node, int weight) { this.node = node;
+            this.node = node;
             this.weight = weight;
         }
     }
+
     static class Edge implements Comparable<Edge> {
         int node1;
         int node2;
         int weight;
+
         Edge(int node1, int node2, int weight) {
             this.node1 = node1;
             this.node2 = node2;
             this.weight = weight;
         }
+
         @Override
         public int compareTo(Edge other) {
             return Integer.compare(this.weight, other.weight);
